535e9c021dea90c634f215d1307858753670a00d
7beelzebub7 <rc0870984@gmail.com>
2025-05-08 21:02:39 -0500

    agrego cambios a las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ export default function App (){
   return(
     <>
       <div className="bg-green-200">
-          <div className="text-3xl text-green-700 text-center font-extrabold ">
-            <p className="p-10">Diferencias entre un sapo de cuevas y un sapo de por ahí</p>
-            <img className="w-full rounded-2xl shadow-2xl" src="images/image01.png" alt="" />
+          <div className=" ">
+            <p className="text-3xl text-green-700 text-center font-extrabold p-10">Diferencias entre un sapo de cuevas y un sapo de por ahí</p>
+            <img className="w-full shadow-2xl p-20 rounded-2xl" src="images/image01.png" alt="" />
           </div>
         
         
           <div className="flex flex-col md:flex-row md:flex-wrap">
-            <div className="bg-green-950 md:basis-1/2 lg:basis-1/2 xl:basis-1/2 p-5">
-              <img className="w-full p-5 rounded-2xl shadow-2xl" src="images/sapoDeCueva.jpg" alt="Sapo de las cuevas" />
+            <div className="bg-green-950 md:basis-1/2 lg:basis-1/2 xl:basis-1/2 p-5 flex flex-col items-center">
+              <img className="w-180 p-5 rounded-2xl shadow-2xl" src="images/sapoDeCueva.jpg" alt="Sapo de las cuevas" />
               <p className="text-xl text-green-50 font-semibold text-center p-7">Este es un sapo de Cueva ¿Diferencia de otros sapos? este vive en als cuevas, este no anda por ahí.</p>
             </div>
             <div className="bg-green-950 md:basis-1/2 lg:basis-1/2 xl:basis-1/2 p-10 flex flex-col items-center">
@@ -30,7 +30,7 @@ export default function App (){
               <img className="rounded-2xl shadow-xl" src="images/sapoReflexivo.jpg" alt="Sapo Reflexivo" />
               <p className="text-xl font-bold text-center p-4">Se volvería un sapo reflexivo ya qué la información para procesar al salir por ahí despues de haber estado toda su vida dentro de la cueva puede ser mucha</p>
             </div>
-            <div className="bg-green-400 md:basis-3/5 lg:basis-full xl:basis-1/3 p-5 rounded-2xl">
+            <div className="bg-green-400 md:basis-3/5 lg:basis-full xl:basis-1/3 p-5 rounded-2xl flex flex-col items-center">
               <img className="rounded-2xl shadow-xl"src="images/sapoC.jpg" alt="Sapo en cueva Artificial" />
               <p className="text-xl font-bold text-center p-4">Se crearía una cueva artificial en lo que puede volver a su cueva natal</p>
             </div>
